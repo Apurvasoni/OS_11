4022dd080109a583fd671503f6abdd4132558e1c
Apurvasoni <49817564+Apurvasoni@users.noreply.github.com>
2019-04-25 22:11:38 +0530

    Add files via upload

--- ca2.c
+++ ca2.c
@@ -1,169 +1,183 @@
-
-
-
 #include<stdio.h>
 #include<stdlib.h>
-void print(int a[][20],int p, int q)
+
+void print_op (int a[][20], int p, int q)
 {
-	int i;
-	int j;
-	for(i=0;i<p;i++)
+	int f;
+	int e;
+	for(f=0;f<p;f++)
 	{
 		printf("\n\t");
-		for(j=0;j<q;j++)
+		for(e=0;e<q;e++)
 		{
-			printf("%d\t",a[i][j]);
+			printf("%d\t",a[f][e]);
 		}
-	}
+	}	
 }
-void resource(int allocation[20][20],int need[20][20],int avail[20][20],int p_id,int q)
+
+//resource request allocation function
+void request_r(int allocate[20][20],int need[20][20],int avail[20][20],int p_id,int  q)
 {
 	int request_m [1][20];
-	int i;
-	printf("\nEnter additional request,if you want:-\n");
-	for(i=0;i<q;i++)
+	int g;
+	printf ("\n Please enter additional request, if you want :- \n");
+	for(g=0 ;g<q;g++)
 	{
-		printf("Request for resource,if necessary %d:-",i+1);
-		scanf("%d",&request_m[0][i]);
+		printf (" Enter your request for resource_%d : ",g+1);
+		scanf ("%d", &request_m[0][g]);
 	}
-	for(i=0;i<q;i++)
-	if(request_m[0][i]>need[p_id][i]);
+	
+	for(g=0;g<q;g++)
+		if(request_m[0][g] > need[p_id][g])// Request is more in comparison to need  
+		{
+			printf("\n Error Found\n");
+			exit(0);
+		}
+
+	for(g=0;g<q;g++)
+		if(request_m[0][g] > avail[0][g])// there is a shortage of resources as available is less and req is more
+		{
+			printf("\n Resources are not available.\n");
+			exit(0);
+		}
+	
+	for(g=0;g<q;g++)  
 	{
-		printf("\nError Found\n");
-		exit(0);
-	}
-	for(i=0;i<q;i++)
-	if(request_m[0][i]>avail[0][i])
-	{
-		printf("Resources are not available\n");
-		exit(0);
-	}
-	for(i=0;i<q;i++)
-	{
-		avail[0][i]-=request_m[0][i];
-		allocation[p_id][i]+=request_m[0][i];
-		need[p_id][i]-=request_m[0][i];
+		avail[0][g]-=request_m[0][g];
+		allocate[p_id][g]+=request_m[0][g];
+		need[p_id][g]-=request_m[0][g];
 	}
 }
-int s_algo(int allocation[][20],int need[][20],int avail[1][20],int p, int q,int x[])
+
+//safety algorithm function
+int s_algo(int allocate[][20],int need[][20],int avail[1][20],int p,int q,int b[])
 {
-	int i,j,k;
-	int w=0;
-	int F[20],available[1][20];
+
+	int s,t,k;
+	int x=0;
+	int T[20],available[1][20];
 	int sf=0,ff=0;
-	for(i=0;i<p;i++)
-	F[i]=0;
-	for(i=0;i<q;i++)
-	available[0][i]=avail[0][i];
+	for(s=0;s<p;s++)
+		T[s]=0;
+	for(s=0;s<q;s++)
+		available[0][s]=avail[0][s];
+
 	for(k=0;k<p;k++)
 	{
-		for(i=0;i<p;i++)
+		for(s=0;s<p;s++)
 		{
-			if(F[i]==0)
+			if(T[s] == 0)
 			{
 				ff=0;
-				for(j=0;j<q;j++)
+				for(t=0;t<q;t++)
 				{
-					if(need[i][j]>available[0][j])
-					ff=1;
+					if(need[s][t] > available[0][t])
+						ff=1;
 				}
-				if(ff==0&&F[i]==0)
+				if(ff == 0 && T[s] == 0)
 				{
-					for(j=0;j<q;j++)
-					available[0][j]+=allocation[i][j];
-					F[i]=1;
+					for(t=0;t<q;t++)
+						available[0][t]+=allocate[s][t];
+					T[s]=1;
 					sf++;
-					x[w++]=i;
+					b[x++]=s;
 				}
 			}
 		}
-		if(sf==p)
-		return 1;
+		if(sf == p)
+			return 1;
 	}
 	return 0;
 }
-void input_information(int allocation[][20],int need[][20],int max[20][20],int available[1][20],int *p,int *q)
+
+
+
+void ip(int allocate[][20],int need[][20],int max[20][20],int available[1][20],int *p,int *q)
 {
-	int i,j;
-	printf("\nWrite the number of processes you want: ");
+	int x,y;
+	printf("\n Total no. of processes are required, please enter : ");
 	scanf("%d",p);
-	printf("\nWrite the number of resources you want: ");
+	printf("\n Total no. of resources are required,please enter: ");
 	scanf("%d",q);
-	for(i=0;i<*p;i++)
+	for(x=0;x<*p;x++)
 	{
-		printf("\nProcess_number\t %d\n",i+1);
-		for(j=0;j<*q;j++)
+		printf("\n Process %d\n",x+1);
+		for(y=0;y<*q;y++)
 		{
-			printf("Enter the allocation for resource_%d :",j+1);
-			scanf("%d",&allocation[i][j]);
-			printf("Enter the maximum for resource_%d :",j+1);
-			scanf("%d",&max[i][j]);
+			printf("Your allocation for resource_%d : ",y+1);
+			scanf("%d",&allocate[x][y]);
+			printf("Your maximum for resource_%d : ",y+1);
+			scanf("%d",&max[x][y]);
 		}
 	}
-	printf("\n Enter the number of available resources below:-\n");
-	for(i=0;i<*q;i++)
-	{
-		printf("Resource %d:",i+1);
-		scanf("%d",&available[0][i]);
+	printf("\nNo. of available resources : \n");
+	for(x=0;x<*q;x++){
+		printf(" Resource_%d : ",x+1);
+		scanf("%d",&available[0][x]);
 	}
-	for(i=0;i<*p;i++)
-	   for(j=0;j<*q;j++)
-	       need[i][j]=max[i][j]-allocation[i][j];
-	printf("\nHere is our Allocation matix");
-	print(allocation,*p,*q);
-	printf("\nMaximum requirement matrix is as follows");
-	print(max,*p,*q);
-	printf("\nAnd the Need matrix is as follows");
-	print(need,*p,*q);
+
+	for(x=0;x<*p;x++)
+		for(y=0;y<*q;y++)
+			need[x][y]=max[x][y]-allocate[x][y];
+
+	printf("\n Here is your allocation matrix");
+	print_op(allocate,*p,*q);
+	printf("\n Your Maximum Requirement Matrix");
+	print_op(max,*p,*q);
+	printf("\n Your Need Matrix");
+	print_op(need,*p,*q);
+
 }
-	int b_algo(int allocation[][20],int need[][20],int available[1][20],int p, int q)
+
+
+//banker's algorithm
+int bk_algo(int allocate[][20],int need[][20],int available[1][20],int p,int q)
+{
+	int j,i,b[10];
+	j=s_algo(allocate,need,available,p,q,b);
+	if(j != 0 )
 	{
-		int i,j,a[20];
-		j=s_algo(allocation,need,available,p,q,a);
-		if(j!=0)
-		{
-			printf("\n\n");
-			printf("\n Safety sequence found and sequence is:-");
-			for(i=0;i<p-1;i++)
-			  printf("P%d->",a[i]);
-			printf("P%d",a[p-1]);
-			return 1;
-		}
-		else
-		{
-			printf("\nDeadlock Occured!!!!\n");
-			return 0;
-		}
+		printf("\n\n");
+		printf("\n Safety sequence found and the sequence is :");
+		for(i=0;i<p-1;i++)
+		     printf(" P%d -> ",b[i]);
+		printf("P%d",b[p-1]);
+		return 1;
 	}
-	int main()
+	else
 	{
-		int op;
-		int allocation[20][20];
-		int max[20][20];
-		int need[20][20];
-		int available[1][20];
-		int p,q;
-		int p_id,ch;
-		printf("\n\t\t\t\t\t\t\tBANKER's ALGORITHM\n");
-		printf("\n\t\t***************************************************************************************************************\n");
-		input_information(allocation,need,available,max,&p,&q);
-		op=b_algo(allocation,need,available,p,q);
-		if(op!=0)
-		{
-			printf("\n\nDo you want to make additional request? (1=yes|0=no)");
-			scanf("%d",ch);
-			if(ch==1)
-			{
-				printf("\n Enter process no.:-");
-				scanf("%d",&p_id);
-				resource(allocation,need,available,p_id-1,q);
-				op=b_algo(allocation,need,available,p,q);
-				if(op==0)
-				 exit(0);
-			}
-		}
-		else
-		 exit(0);
+		printf("\n !!Deadlock occured!!\n");
 		return 0;
 	}
+}
+
+
+int main(){
+	int op;
+	int allocate[20][20];
+	int max[20][20];
+	int need[20][20];
+	int available[1][20];
+	int p,q,p_id,ch;
+	printf("\n \t ********************* BANKER'S ALOGORITHM **************************\n");
+	ip(allocate,need,max,available,&p,&q);
+	op=bk_algo(allocate,need,available,p,q);
+	if(op !=0 ){
+		printf("\n\n Do you want make additional request ? (1=yes|0=no)");
+		scanf("%d",&ch);
+		if(ch == 1)
+		{
+			printf("\n Please enter your process no. : ");
+			scanf("%d",&p_id);
+			request_r(allocate,need,available,p_id-1,q);
+			op=bk_algo(allocate,need,available,p,q);
+			if(op == 0 )
+				exit(0);
+		}
+	}
+	else
+		exit(0);
+	return 0;
+}
+
 
